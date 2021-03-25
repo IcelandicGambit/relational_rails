e379b3f39019c9385f1c77c643d42fd3a28a7ada
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-24 19:26:47 -0700

    add authors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_012627) do
+ActiveRecord::Schema.define(version: 2021_03_25_022435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.boolean "is_alive?"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "manufacturers", force: :cascade do |t|
     t.string "name"
