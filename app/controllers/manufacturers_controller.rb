8a46807072abcca94e5631bf8db584f520213f07
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-25 21:08:18 -0700

    rebase

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,5 +1,10 @@
 class ManufacturersController < ApplicationController
   def index
-    
+    @manufacturers = Manufacturer.all
+  end
+
+  def show
+    #binding.pry
+    @manufacturer = Manufacturer.find(params[:id])
   end
 end
