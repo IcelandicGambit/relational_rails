fc7c794f7641bfa3deddee2fd0a29d9ec7c11d57
Joey Haas <josephthaas@gmail.com>
2021-03-28 14:02:19 -0600

    completed model test

---
+++ spec/models/manufacturer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Manufacturer, type: :model do
+  describe 'relationships' do
+    it { should have_many :bicycles }
+  end
+
+  describe 'instance methods' do
+    describe 'count_by_manufacturer' do
+      it 'number of bicycles associated with manufacturer' do
+        m_1 = Manufacturer.create!(name: "Niner", year_founded: 1990, manufactured_in_us?: true)
+        m_2 = Manufacturer.create!(name: "Trek", year_founded: 1922, manufactured_in_us?: false)
+        b_1 = m_1.bicycles.create!(model: "Wander Lust", price: 1990, has_rack_mount: true)
+        b_2 = m_2.bicycles.create!(model: "Bomber", price: 1922, has_rack_mount: false)
+        b_3 = m_1.bicycles.create!(model: "Rove Ti", price: 1811, has_rack_mount: true)
+        b_4 = m_2.bicycles.create!(model: "Coiler", price: 1211, has_rack_mount: false)
+
+        expect(m_1.count_by_manufacturer).to eq(2)
+        expect(m_2.count_by_manufacturer).to eq(2)
+      end
+    end
+  end
+end
