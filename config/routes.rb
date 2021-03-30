cf618e9deea75ea7fd0bf9f01d9976798954a679
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-30 08:07:05 -0700

    more routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/authors/new', to: 'authors#new'
   get '/authors/:id/edit', to: 'authors#edit'
   patch '/authors/:id', to: 'authors#update'
-  post '/authors', to: 'authors#add'
+  post '/authors', to: 'authors#create'
   get '/authors/:id', to: 'authors#show'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
