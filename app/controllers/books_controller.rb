bdb65ca5d204e2b3f3a5e39eb937c035589b43ac
Andrew Smith <andrewpatrick138@gmail.com>
2021-03-30 15:56:31 -0700

    rbicycle controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,20 @@ class BooksController < ApplicationController
   def show
      @books = Book.find(params[:id])
   end
+
+  def new
+    @author = Author.find(params[:id])
+  end
+
+  def create
+
+    author = Author.find(params[:id])
+    author.books.create(book_params)
+    redirect_to "/authors/#{author.id}/book_listing"
+  end
+
+  private
+  def bicycle_params
+    params.permit(:title, :number_of_pages, :digital)
+  end
 end
